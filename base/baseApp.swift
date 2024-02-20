614510c644f514c5f02e0064c092b45c45ae6e07
Le anh Duc <lduc@ampol.com.au>
2024-02-20 15:14:28 +0700

    initial project

---
+++ base/baseApp.swift
@@ -0,0 +1,17 @@
+//
+//  baseApp.swift
+//  base
+//
+//  Created by AnhDuc on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct baseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
